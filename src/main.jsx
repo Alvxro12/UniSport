0081cd2bb66fdd4e1cf2388825a1be266099b372
Alvxro12 <Ajimenezfornaris@gmail.com>
2025-05-26 21:30:07 -0500

    Arreglando rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import App from './App.jsx'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={import.meta.env.MODE === 'production' ? '/UniSport' : '/'}>
+  <HashRouter basename={import.meta.env.MODE === 'production' ? '/UniSport' : '/'}>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/UniSport/' : '/',
-  plugins: [react(), tailwindcss(),],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/UniSport/' : '/',
+    plugins: [react(), tailwindcss()],
+  }
 })
